477c7433ab6e11438305f28a925c5108e5de5810
Wiktork <wiktor.kepczynski@adfinis.com>
2020-09-21 15:12:50 +0200

    fix broken selenium tests due to previous changes in code

--- osschallenge/tests/test_selenium_no_users_in_ranking.py
+++ osschallenge/tests/test_selenium_no_users_in_ranking.py
@@ -1,6 +1,9 @@
 from osschallenge.tests.pages.ranking import RankingPage
+from osschallenge.tests.pages.login import LoginPage
 from django.test import Client
 from osschallenge.tests.selenium_test_options import SeleniumTests
+from . import factories
+from django.contrib.auth.models import Group
 
 
 class NoUsersInRankingTest(SeleniumTests):
@@ -11,8 +14,20 @@ class NoUsersInRankingTest(SeleniumTests):
 
     @classmethod
     def setUp(self):
-        self.ranking_page = RankingPage(self.driver, self.live_server_url)
         self.client = Client()
+        self.ranking_page = RankingPage(self.driver, self.live_server_url)
+        self.login_page = LoginPage(self.driver, self.live_server_url)
+        self.user1 = factories.UserFactory(username="Test")
+        self.user1.set_password("12345qwert")
+        self.user1.save()
+        self.group2 = Group.objects.create(
+            id = 1,
+            name = "Mentor"
+        )
+        self.login_page.open()
+        self.login_page.login("Test", "12345qwert")
+        self.client.login(username="Test", password='klajsdfkj')
+        self.driver.get(self.live_server_url + '/login/')
 
     @classmethod
     def tearDownClass(self):
@@ -21,5 +36,6 @@ class NoUsersInRankingTest(SeleniumTests):
 
     def test_ranking_with_no_users(self):
         self.ranking_page.open('/ranking/?page=100')
+        self.group2.user_set.add(self.user1)
         message = self.ranking_page.find_message()
         self.assertEquals(message.text, 'No users available')
