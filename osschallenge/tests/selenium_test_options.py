dd74ac3c37d74f10d42ae6a7edeafef412118435
Wiktork <wiktor.kepczynski@adfinis.com>
2020-11-18 08:26:00 +0100

    fix: add webdriver manager to manage webdriver version

--- osschallenge/tests/selenium_test_options.py
+++ osschallenge/tests/selenium_test_options.py
@@ -1,5 +1,6 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 class SeleniumTests(StaticLiveServerTestCase):
@@ -11,5 +12,5 @@ class SeleniumTests(StaticLiveServerTestCase):
         options.add_argument('headless')
         options.add_argument('no-sandbox')
         options.add_argument('window-size=1200x600')
-        self.driver = webdriver.Chrome(chrome_options=options)
+        self.driver = webdriver.Chrome(chrome_options=options, ChromeDriverManager().install())
         self.driver.implicitly_wait(10)
